454a79a9652ab04fb37d2f5e0d80597f7ce7ecc5
Manglam <134299788+DevManglam@users.noreply.github.com>
2023-12-26 16:53:00 +0530

    Update EcoPlugin.java

--- src/main/java/me/manglam/ecoplugin/EcoPlugin.java
+++ src/main/java/me/manglam/ecoplugin/EcoPlugin.java
@@ -1,17 +1,28 @@
 package me.manglam.ecoplugin;
 
+import me.manglam.ecoplugin.commands.EconomyCommand;
+import me.manglam.ecoplugin.listener.PlayerJoinListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class EcoPlugin extends JavaPlugin {
+public class EcoPlugin extends JavaPlugin {
+
+    private static EcoPlugin instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        getCommand("eco").setExecutor(new EconomyCommand());
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+    }
+
+    public static EcoPlugin getInstance() {
+        return instance;
     }
 }
+
